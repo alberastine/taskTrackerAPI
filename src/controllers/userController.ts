29e27f787576040d84a262707e0898e14e72bb09
alberastine <126144757+alberastine@users.noreply.github.com>
2025-06-19 10:28:21 +0800

    deploy test 4

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -72,8 +72,8 @@ export const loginUser = async (req: Request, res: Response) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'lax',
+            secure: true,
+            sameSite: 'none',
             maxAge: 3600000,
         });
 
